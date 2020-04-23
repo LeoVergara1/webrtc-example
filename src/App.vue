180b1ac81198c2f79593be2e28735f7f62f45a48
Brandon Vergara <brandon@makingdevs.com>
2020-04-23 12:56:29 -0500

    coding master  adding component and url socker heroku

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,75 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+              <vue-webrtc ref="webrtc"
+                      width="100%"
+                      :roomId="roomId"
+                      v-on:joined-room="logEvent"
+                      v-on:left-room="logEvent"
+                      v-on:open-room="logEvent"
+                      v-on:share-started="logEvent"
+                      v-on:share-stopped="logEvent"
+                      @error="onError" />
+     <div class="row">
+          <div class="col-md-12 my-3">
+            <button type="button" class="btn btn-primary" @click="onJoin">Join</button>
+            <button type="button" class="btn btn-primary" @click="onLeave">Leave</button>
+            <button type="button" class="btn btn-primary" @click="onCapture">Capture Photo</button>
+            <button type="button" class="btn btn-primary" @click="onShareScreen">Share Screen</button>
+          </div>
+      </div>
+      <div class="row">
+      <div class="col-md-12">
+        <h2>Captured Image</h2>
+        <figure class="figure">
+          <img :src="img" class="img-responsive" />
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import {WebRTC} from 'vue-webrtc'
+Vue.component(WebRTC.name, WebRTC);
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data: function () {
+    return {
+      img: null,
+      roomId: "public-room"
+    }
+  },
+  created: function(){
+    console.log("Init")
+    console.log(this.$refs)
+  },
+  methods: {
+      onCapture() {
+        this.img = this.$refs.webrtc.capture();
+      },
+      onJoin() {
+        this.$refs.webrtc.join();
+      },
+      onLeave() {
+        this.$refs.webrtc.leave();
+      },
+      onShareScreen() {
+        this.img = this.$refs.webrtc.shareScreen();
+      },
+      onError(error, stream) {
+        console.log('On Error Event', error, stream);
+      },
+      logEvent(event) {
+        console.log('Event : ', event);
+      },
   }
 }
 </script>
